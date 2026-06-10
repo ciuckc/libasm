6906b0612413d110f831bf22fffc4f2cc65af552
scristia <scristian@student.codam.nl>
2026-06-10 19:55:02 +0200

    feat: add ft_strlen tests

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 extern size_t ft_strlen(const char* str);
 
-int main() {
-    printf("%zu", ft_strlen("Hello, world!"));
+static void test_ft_strlen(void)
+{
+    assert(ft_strlen("Hello, world!") == strlen("Hello, world!"));
+    assert(ft_strlen("") == 0);
+    assert(ft_strlen("a") == 1);
+    assert(ft_strlen("abc") == 3);
+    printf("ft_strlen: OK\n");
+}
+
+int main(void)
+{
+    test_ft_strlen();
 }
